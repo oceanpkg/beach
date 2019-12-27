78ac587fd25ede46d3f39a0d8311848fb9ce8b1e
Nikolai Vazquez <nikvzqz@gmail.com>
2019-12-27 00:39:48 -0500

    Create `beach` crate

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! Sandboxing utilities for Ocean.
+//!
+//! This project is a work in progress. If you want to help make this a reality,
+//! contact [Nikolai Vazquez].
+//!
+//! [Nikolai Vazquez]: https://twitter.com/NikolaiVazquez
+
+#![deny(missing_docs)]
